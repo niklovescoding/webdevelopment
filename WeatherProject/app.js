bcceeb46c01a9d1095eb127fdd0274f87268a22c
nikhilcodess <nikhilkaushal0000@gmail.com>
2020-08-05 21:00:07 +0530

    Completed Weather App

---
+++ WeatherProject/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser")
+const app  = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res)
+{
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res)
+{
+      const query = req.body.cityName;
+      const key = "db2d48b0d37233298b6ccf42c972c02d";
+      const unit = "metric";
+      const url  = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + key + "&units=" + unit;
+      https.get(url, response =>
+      {
+          console.log(response.statusCode);
+
+          response.on("data", function(data)
+          {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon
+            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            res.write("<p>Currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in "+ query +" is " + temp + " degrees Celcius.</h1>");
+            res.write("<img src = " + imgURL + ">");
+            res.send();
+          })
+      })
+  })
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Started on port 3000")
+})
---
+++ calculator/calculator.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1+num2
+    res.send("Result is " + result)
+})
+app.get("/bmicalculate", function(req,res){
+	res.sendFile(__dirname + "/bmiCalculator.html")
+})
+app.post("/bmicalculate", function(req,res){
+	var hei = parseFloat(req.body.height);
+	var wei = parseFloat(req.body.weight);
+	var bmi = (wei/(hei*hei));
+	res.send("Your BMI is " + bmi);
+})
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
